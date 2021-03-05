289eef85678a3f654e578b3426bfb3dffc66c991
kumar <kumar@kali.kumar>
2021-03-05 23:13:27 +0530

    Updates

---
+++ precache-manifest.8c4af9b0ce63e67d6666be31fa07dff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afc4386760900b6f40d3fd83b8b6c6d",
+    "url": "/clone-netflix/index.html"
+  },
+  {
+    "revision": "e1be041ceb48867b7eee",
+    "url": "/clone-netflix/static/css/main.496bfedb.chunk.css"
+  },
+  {
+    "revision": "8a1e732da22da6a417e8",
+    "url": "/clone-netflix/static/js/2.043bb1b9.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/clone-netflix/static/js/2.043bb1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1be041ceb48867b7eee",
+    "url": "/clone-netflix/static/js/main.128a03b6.chunk.js"
+  },
+  {
+    "revision": "959c2486cb1511e0d39c",
+    "url": "/clone-netflix/static/js/runtime-main.73cee1ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clone-netflix/static/media/logo.5d5d9eef.svg"
+  }
+]);
